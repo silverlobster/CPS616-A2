991052720ca7a3be55e67b84469099184cae9462
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2022-03-11 22:20:35 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ def increment_by_two(var_to_increment):
     """
     This function increments input by two
     :param var_to_increment: param to increment
-    :return: incremented variable ddddddeeeeeeeeeeee
+    :return: incremented variable ddddddeeeeeeeeeeeesaxdas dassadada
     """
     return var_to_increment + 2
 
